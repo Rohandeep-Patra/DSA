8a5b3fe9960d042c8765977c7778cda323607d49
Rohandeep Patra <rohandp100@gmail.com>
2024-05-13 14:09:14 +0530

    Update Patten 2

--- 1. Build Logical Thinking ( Patterns )/pattern2.java
+++ 1. Build Logical Thinking ( Patterns )/pattern2.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 public class pattern2 {
-    static void pattern(int n){
-        int i,j;
-        for(i = 0;i<n;i++){
-            for(j = 0;j<i+1;j++){
+    static void pattern(int n) {
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
