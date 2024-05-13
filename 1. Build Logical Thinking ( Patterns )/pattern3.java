e008232756bf0280472f14a298f8456e7eeef10b
Rohandeep Patra <rohandp100@gmail.com>
2024-05-13 14:08:11 +0530

    Write Pattern 3

---
+++ 1. Build Logical Thinking ( Patterns )/pattern3.java
@@ -0,0 +1,20 @@
+import java.util.*;;
+
+public class pattern3 {
+    static void pattern(int n) {
+        int i, j;
+        for (i = 1; i < n+1; i++) {
+            for (j = 1; j < i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        pattern(num);
+        sc.close();
+    }
+}
